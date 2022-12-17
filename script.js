90af5ada243394838113005c4ac2005338134e41
izazw <iza@wasielak.eu>
2022-12-17 17:44:34 -0500

    basic html + styles structure

---
+++ script.js
@@ -0,0 +1,16 @@
+//grab span element
+
+//grab button + element
+//add amount to the button element and update span element
+//grab button -element
+//decrement amount - update span element
+//grab button "random" element
+//generate random number and update span element
+
+//maybe:
+// stop from removing amount of kittens below zero
+
+
+
+
+
